e61eeeb68a57ad0268d201c63cc2b4c08a470cc6
HANTAEDONG <htd0913@gmail.com>
2024-11-07 16:35:20 +0900

    feat: slice 작성

--- src/store/slices/boardSlice.ts
+++ src/store/slices/boardSlice.ts
@@ -1,8 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardState = {
+  modalActive: boolean;
+  boardArray: IBoard[];
+};
+
+const initialState: TBoardState = {
   modalActive: false,
-  boardArray: [],
+  boardArray: [
+    {
+      boardId: "board-0",
+      boardName: "첫 번째 게시물",
+      lists: [
+        {
+          listId: "list-0",
+          listName: "List 1",
+          tasks: [
+            {
+              taskId: "task-0",
+              taskName: "Task 1",
+              taskDescription: "Task 1",
+              taskOwner: "John",
+            },
+            {
+              taskId: "task-1",
+              taskName: "Task 2",
+              taskDescription: "Task 2",
+              taskOwner: "Han",
+            },
+          ],
+        },
+        {
+          listId: "list-1",
+          listName: "List 2",
+          tasks: [
+            {
+              taskId: "task-2",
+              taskName: "Task 3",
+              taskDescription: "Task 3",
+              taskOwner: "Kim",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 const boardSlice = createSlice({
--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,22 @@ export type TLogItem = {
   logMessage: string;
   logTimestamp: string;
 };
+
+export interface IBoard {
+  boardId: string;
+  boardName: string;
+  lists?: IList[];
+}
+
+export interface IList {
+  listId: string;
+  listName: string;
+  tasks: ITask[];
+}
+
+export interface ITask {
+  taskId: string;
+  taskName: string;
+  taskDescription: string;
+  taskOwner: string;
+}
